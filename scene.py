cf7f875c7cce8939b00c00daf9b1692157d44dc3
Tom Genlis <bryckout@gmail.com>
2023-08-05 00:56:49 +0200

    test(scene): agent position testing

--- scene.py
+++ scene.py
@@ -5,20 +5,26 @@ from world_objects.voxel_marker import VoxelMarker
 from world_objects.water import Water
 from world_objects.clouds import Clouds
 from agent import Agent
-
+import logging
 
 class Scene:
     def __init__(self, app):
+        logging.info("Initializing scene...")
         self.app = app
+        logging.info("Initializing world...")
         self.world = World(self.app)
+        logging.info("Initializing voxel marker...")
         self.voxel_marker = VoxelMarker(self.world.voxel_handler)
+        logging.info("Initializing water...")
         self.water = Water(app)
+        logging.info("Initializing clouds...")
         self.clouds = Clouds(app)
         
+        logging.info("Initializing agents...")
         # Agents
         self.agents = []
         for _ in range(1):
-            self.agents.append(Agent(self.world.voxel_handler, PLAYER_POS + glm.vec3(0, 50, -10), glm.vec2(0, 0)))
+            self.agents.append(Agent(self.world.voxel_handler, PLAYER_POS + glm.vec3(0, 500, -10), glm.vec2(0, 0)))
 
     def update(self, dt):
         self.world.update()
@@ -33,10 +39,6 @@ class Scene:
         # chunks rendering
         self.world.render()
 
-        # agents rendering
-        for agent in self.agents:
-            agent.render()
-
         # rendering without cull face
         self.app.ctx.disable(mgl.CULL_FACE)
         self.clouds.render()
@@ -45,3 +47,7 @@ class Scene:
 
         # voxel selection
         self.voxel_marker.render()
+
+        # agents rendering
+        for agent in self.agents:
+            agent.render()
