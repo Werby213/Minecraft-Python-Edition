3254b1b547eec96fca842a7ffb72062a3878e842
Tom Genlis <bryckout@gmail.com>
2023-08-04 02:39:41 +0200

    feat(agent): added agents to scene

--- scene.py
+++ scene.py
@@ -4,6 +4,7 @@ from world import World
 from world_objects.voxel_marker import VoxelMarker
 from world_objects.water import Water
 from world_objects.clouds import Clouds
+from agent import Agent
 
 
 class Scene:
@@ -13,15 +14,28 @@ class Scene:
         self.voxel_marker = VoxelMarker(self.world.voxel_handler)
         self.water = Water(app)
         self.clouds = Clouds(app)
+        
+        # Agents
+        self.agents = []
+        for _ in range(1):
+            self.agents.append(Agent(self.world.voxel_handler, PLAYER_POS + glm.vec3(0, 50, -10), glm.vec2(0, 0)))
 
-    def update(self):
+    def update(self, dt):
         self.world.update()
+
+        for agent in self.agents:
+            agent.update(dt)
+
         self.voxel_marker.update()
         self.clouds.update()
 
     def render(self):
         # chunks rendering
         self.world.render()
+
+        # agents rendering
+        for agent in self.agents:
+            agent.render()
 
         # rendering without cull face
         self.app.ctx.disable(mgl.CULL_FACE)
